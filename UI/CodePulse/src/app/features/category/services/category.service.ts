0d57299c2e81170239320a9ab627ec85ad5e9c3a
Samantha Sutter <sutters4@gmatc.matc.edu>
2023-12-03 19:09:39 -0600

    Finishing touches

--- UI/CodePulse/src/app/features/category/services/category.service.ts
+++ UI/CodePulse/src/app/features/category/services/category.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-request.model';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http'
+import { Category } from '../models/category.model'
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +14,10 @@ export class CategoryService {
 
   addCategory(model: AddCategoryRequest): Observable<void> {
     return this.http.post<void>('https://localhost:7226/api/categories', model);
+  }
+
+  getAllCategories(): Observable<Category[]>{
+    return this.http.get<Category[]>('https://localhost:7226/api/Categories');
   }
 
 }
